6a9b1162722382ceca25166aa9e46468b38250ba
0498drew <mwaseandrew98@gmail.com>
2022-09-26 19:32:42 -0700

    python inheretance

---
+++ 0x0A-python-inheritance/tests/1-my_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Defines an inheretance class""
+
+
+class MYList(list):
+    """class of list"""
+
+    def print_sorted(self):
+    """sprint in ascending order"""
+    print(sorted(self)
