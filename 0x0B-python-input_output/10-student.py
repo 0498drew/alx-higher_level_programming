f4145d9032e5c7accb1b96624e74f842f71497bb
0498drew <mwaseandrew98@gmail.com>
2022-09-30 17:59:52 -0700

    student

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -20,6 +20,6 @@ class Student():
     def to_json(self, attrs=None):
         """Dict representation of student class"""
         if (type(attrs) == list and
-                all(type(ele) == str for ele in attrs)): return {k:
-                        getattr(self, k) for k in attrs if hasattr(self, k)}
+                all(type(ele) == str for ele in attrs)):
+            return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
         return self.__dict__
