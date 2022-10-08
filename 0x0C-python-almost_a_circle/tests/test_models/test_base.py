45755ead407bbf0c5c40e95c62659b13824bfa1f
0498drew <mwaseandrew98@gmail.com>
2022-10-08 02:53:50 -0700

    almost a circle

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -14,12 +14,11 @@ import os
 import unittest
 from models.base import Base
 from models.rectangle import Rectangle
-from models.square import Square"""
-
+from models.square import Square
 
 
 class TestBase_instantiation(unittest.TestCase):
-    """Unittests for testing instantiation of the Base class."""
+    """Unittests for testing instantiation of the Base class"""
 
     def test_no_arg(self):
         b1 = Base()
