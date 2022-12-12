b0a017bbc1491956cca9e1d0e50124f02f46f391
0498drew <mwaseandrew98@gmail.com>
2022-12-12 04:26:55 -0800

    javascript

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,62 @@
+#!/usr/bin/node
+const SquareModel = require('./5-square');
+
+module.exports = class Square extends SquareModel {
+  constructor (size) {
+    super(size, size);
+  }
+
+  charPrint (c) {
+    this.print(c);
+  }
+};
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+"6-square.js" [New File]                                          0,0-1         All
+
